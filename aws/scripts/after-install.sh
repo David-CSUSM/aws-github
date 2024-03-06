74abee9f35fd7858feac8f49e24b3fb0854018a9
David-CSUSM <herna1334@csusm.edu>
2024-03-06 10:22:18 -0800

    try fix

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -69,10 +69,12 @@ EOF
 # Install dependencies
 # --------------------------------------------------------------------------------
 
-python3 -m ensurepip --upgrade && pip3 install --upgrade pip
+python3 -m ensurepip --upgrade
+
+pip install --upgrade pip
 
 python3 -m venv .venv
 
 source .venv/bin/activate
 
-pip3 install -r requirements.txt
+pip install -r requirements.txt

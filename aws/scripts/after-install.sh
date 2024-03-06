2867e3e63b2ad36553972117cd0609da5f589102
David-CSUSM <herna1334@csusm.edu>
2024-03-06 00:08:28 -0800

    add create .env

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -1,9 +1,12 @@
 echo "after install"
 SECRET_KEY=$(aws ssm get-parameters --region us-east-1 --names SECRET_KEY --with-decryption --query Parameters[0].Value)
-echo $SECRET_KEY
 
 DEBUG=$(aws ssm get-parameters --region us-east-1 --names DEBUG --with-decryption --query Parameters[0].Value)
-echo $DEBUG
 
 ALLOWED_HOSTS=$(aws ssm get-parameters --region us-east-1 --names ALLOWED_HOSTS --with-decryption --query Parameters[0].Value)
-echo $ALLOWED_HOSTS
+
+cat > .env <<EOL
+SECRET_KEY=${SECRET_KEY}
+DEBUG=${DEBUG}
+ALLOWED_HOSTS=${ALLOWED_HOSTS}
+EOL

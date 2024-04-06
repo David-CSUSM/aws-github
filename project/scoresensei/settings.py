b95dcf57dc57f674fe3b91ca3f1c9b95504f2245
David-CSUSM <herna1334@csusm.edu>
2024-04-05 22:47:27 -0700

    organize files into main app

--- project/scoresensei/settings.py
+++ project/scoresensei/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -120,10 +121,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-
-STATICFILES_DIRS = [
-    BASE_DIR / "staticfiles",
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
